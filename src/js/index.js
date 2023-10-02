e4d594f999a254d52cccac7935e99eaf5d36743f
cesardailee <122842004+cesardailee@users.noreply.github.com>
2023-10-01 21:51:45 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,39 @@
+/*
+o que precisamos fazer? - quando clicr no botão do personagem na lista, temos que marcar o botão como selecionado e mostrar o personagem correspondente
+
+    OBJETIVO 1 - quando clicar no botão do personagem na lista, marcar o botão como selecionado
+        passo 1 - pegar os botões no js para poder verificar quando o usuário clicar em cima de um deles
+        passo 2 - adicionar a classe "selecionado" no botão que o usuário clicou
+        passo 3 - verificar se já existe um botão selecionado, se sim, devemos removar a seleção deles
+
+    OBJETIVO 2 - quando clicar no botão do personagem mostrar as informações do personagem
+        passo 1 - pegar o personagem no js para poder mostrar ou esconder ele
+        passo 2 - adicionar a classe "selecionado" no personagem que o usuário selecionou
+        passo 3 - verificar se já exixte um personagem selecionado, se sim, devemos remover a seleção dele
+*/
+
+const botoes = document.querySelectorAll('.botao');
+
+const personagens = document.querySelectorAll(".personagem");
+
+
+botoes.forEach((botao, indice) => {
+    botao.addEventListener("click", () => {
+        desselecionarBotao();
+        desselecionarPersonagem();
+
+        botao.classList.add("selecionado");
+        personagens[indice].classList.add("selecionado");
+    });
+})
+
+function desselecionarPersonagem() {
+    const personagemSelecionado = document.querySelector(".personagem.selecionado");
+    personagemSelecionado.classList.remove("selecionado");
+}
+
+function desselecionarBotao() {
+    const botaoselecionado = document.querySelector(".botao.selecionado");
+    botaoselecionado.classList.remove("selecionado");
+}
+
